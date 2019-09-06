c7ad1ba30484eb3de2e4fe1c3675e2a7a0cb137b
Jia Yao Wong <jiayao.wong@DRMAIN.local>
2019-09-06 14:08:52 -0400

    Daddy wong is finished >.o

--- HelloMonkies.java
+++ HelloMonkies.java
@@ -2,6 +2,11 @@ public class HelloMonkies{
 	public static void main(String[] args) {
 	
 		System.out.println("Hello Monkies");
+		this.printMessage();
+	}
+
+	public static void printMessage(){
+		System.out.println("Hentai with senpai????");
 	}
 
 
